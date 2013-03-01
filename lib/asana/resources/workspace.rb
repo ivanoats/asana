8ab7f2430d4e3a2f6a1b606800bd8a72f7daa0cd
Ryan Bright <ryan@rbright.net>
2013-03-01 03:10:38 -0500

    Update the Workspace resource

--- lib/asana/resources/workspace.rb
+++ lib/asana/resources/workspace.rb
@@ -1,5 +1,91 @@
 module Asana
   class Workspace < Resource
+
     COLLECTION = "workspaces"
+
+    class << self
+
+      def all
+        get("#{COLLECTION}")
+      end
+
+      def find(id)
+        get("#{COLLECTION}/#{id}")
+      end
+
+      def update(id, data)
+        put("#{COLLECTION}/#{id}", data)
+      end
+
+      def projects(id)
+        Asana::Project.get("#{COLLECTION}/#{id}/#{Project::COLLECTION}")
+      end
+
+      def create_project(id, data)
+        Asana::Project.post("#{COLLECTION}/#{id}/#{Project::COLLECTION}", data)
+      end
+
+      def tasks(id, params = {})
+        Asana::Task.get("#{COLLECTION}/#{id}/#{Task::COLLECTION}", {
+          :assignee => 'me'
+        }.merge(params))
+      end
+
+      def create_task(id, data)
+        Asana::Task.post("#{COLLECTION}/#{id}/#{Task::COLLECTION}", {
+          :assignee => 'me'
+        }.merge(data))
+      end
+
+      def users(id)
+        Asana::User.get("#{COLLECTION}/#{id}/#{User::COLLECTION}")
+      end
+
+      def tags(id)
+        Asana::Tag.get("#{COLLECTION}/#{id}/#{Tag::COLLECTION}")
+      end
+
+      def create_tag(id, data)
+        Asana::Tag.post("#{COLLECTION}/#{id}/#{Tag::COLLECTION}", data)
+      end
+
+    end
+
+    def update(data)
+      put(data)
+    end
+
+    def projects
+      Asana::Project.get("#{path}/#{Project::COLLECTION}")
+    end
+
+    def create_project(data)
+      Asana::Project.post("#{path}/#{Project::COLLECTION}", data)
+    end
+
+    def tasks(params = {})
+      Asana::Task.get("#{path}/#{Task::COLLECTION}", {
+        :assignee => 'me'
+      }.merge(params))
+    end
+
+    def create_task(data)
+      Asana::Task.post("#{path}/#{Task::COLLECTION}", {
+        :assignee => 'me'
+      }.merge(data))
+    end
+
+    def users
+      Asana::User.get("#{path}/#{User::COLLECTION}")
+    end
+
+    def tags
+      Asana::Tag.get("#{path}/#{Tag::COLLECTION}")
+    end
+
+    def create_tag(data)
+      Asana::Tag.post("#{path}/#{Tag::COLLECTION}", data)
+    end
+
   end
 end
--- spec/asana/resources/workspace_spec.rb
+++ spec/asana/resources/workspace_spec.rb
@@ -15,83 +15,154 @@ module Asana
     end
 
     describe '.all' do
-      it 'should return all of the user\'s workspaces' do
+      it "should return all of the user's workspaces" do
         workspaces = Workspace.all
         workspaces.must_be_instance_of Array
-        workspaces.first.must_be_instance_of Workspace
+        workspaces.last.must_be_instance_of Workspace
       end
     end
 
     describe '.find' do
-      it 'should return a single workspace' do
+      it "should return a single workspace" do
         workspace = Workspace.find(Workspace.all.first.id)
         workspace.must_be_instance_of Workspace
       end
     end
 
-    describe '#create' do
-      it 'should raise an ActiveResource::MethodNotAllowed exception' do
-        workspace = Workspace.new
-        lambda { workspace.save }.must_raise ActiveResource::MethodNotAllowed
-      end
-    end
-
-    describe '#create_task' do
-      it 'should create a new task for the given workspace' do
-        workspace = Workspace.all.first
-        task = workspace.create_task(:name => 'asana-test-task', :assignee => 'me')
-        task.must_be_instance_of Task
-      end
-    end
-
-    describe '#create_project' do
-      it 'should create a new project for the given workspace' do
-        workspace = Workspace.all.first
-        project = workspace.create_project(:name => 'asana-test-project')
-        project.must_be_instance_of Project
-      end
-    end    
-
-    describe '#update' do
-      it 'should update the given workspace with a new name' do
+    describe '.update' do
+      it "should update the given workspace with a new name" do
         workspace = Workspace.all.last
-        workspace.update_attribute(:name, 'foo')
+        Workspace.update(workspace.id, :name => 'foo')
+        workspace = Workspace.find(workspace.id)
         workspace.name.must_equal 'foo'
       end
     end
 
-    describe '#destroy' do
-      it 'should raise an ActiveResource::MethodNotAllowed exception' do
-        workspace = Workspace.all.first
-        lambda { workspace.destroy }.must_raise ActiveResource::MethodNotAllowed
+    describe '.projects' do
+      it "should return all projects for the given workspace" do
+        workspace = Workspace.all.last
+        projects = Workspace.projects(workspace.id)
+        projects.must_be_instance_of Array
+        projects.first.must_be_instance_of Project
+      end
+    end
+
+    describe '.create_project' do
+      it "should create a new project for the given workspace" do
+        workspace = Workspace.all.last
+        project = Workspace.create_project(workspace.id, :name => 'asana-test-project')
+        project.must_be_instance_of Project
+      end
+    end
+
+    describe '.tasks' do
+      it "should return all tasks for the given workspace" do
+        workspace = Workspace.all.last
+        tasks = Workspace.tasks(workspace.id)
+        tasks.must_be_instance_of Array
+        tasks.first.must_be_instance_of Task
+      end
+    end
+
+    describe '.create_task' do
+      it "should create a new task for the given workspace" do
+        workspace = Workspace.all.last
+        task = Workspace.create_task(workspace.id, :name => 'asana-test-task', :assignee => 'me')
+        task.must_be_instance_of Task
+      end
+    end
+
+    describe '.users' do
+      it "should return all users for the given workspace" do
+        workspace = Workspace.all.last
+        users = Workspace.users(workspace.id)
+        users.must_be_instance_of Array
+        users.first.must_be_instance_of User
+      end
+    end
+
+    describe '.tags' do
+      it "should return all tags for the given workspace" do
+        workspace = Workspace.all.last
+        tags = Workspace.tags(workspace.id)
+        tags.must_be_instance_of Array
+        tags.first.must_be_instance_of Tag
+      end
+    end
+
+    describe '.create_tag' do
+      it "should create a new tag for the given workspace" do
+        workspace = Workspace.all.last
+        task = Workspace.create_tag(workspace.id, :name => 'asana-test-tag')
+        task.must_be_instance_of Tag
+      end
+    end
+
+    describe '#update' do
+      it "should update the given workspace with a new name" do
+        workspace = Workspace.all.last
+        workspace.update(:name => 'foo')
+        workspace.name.must_equal 'foo'
       end
     end
 
     describe '#projects' do
-      it 'should return all projects for the given workspace' do
-        workspace = Workspace.all.first
+      it "should return all projects for the given workspace" do
+        workspace = Workspace.all.last
         projects = workspace.projects
         projects.must_be_instance_of Array
         projects.first.must_be_instance_of Project
       end
     end
 
+    describe '#create_project' do
+      it "should create a new project for the given workspace" do
+        workspace = Workspace.all.last
+        project = workspace.create_project(:name => 'asana-test-project')
+        project.must_be_instance_of Project
+      end
+    end
+
     describe '#tasks' do
-      it 'should return all tasks for the given workspace' do
-        workspace = Workspace.all.first
-        user = User.all.first
-        tasks = workspace.tasks(user.id)
+      it "should return all tasks for the given workspace" do
+        workspace = Workspace.all.last
+        tasks = workspace.tasks
         tasks.must_be_instance_of Array
         tasks.first.must_be_instance_of Task
       end
     end
 
+    describe '#create_task' do
+      it "should create a new task for the given workspace" do
+        workspace = Workspace.all.last
+        task = workspace.create_task(:name => 'asana-test-task', :assignee => 'me')
+        task.must_be_instance_of Task
+      end
+    end
+
     describe '#users' do
-      it 'should return all users for the given workspace' do
-        workspace = Workspace.all.first
+      it "should return all users for the given workspace" do
+        workspace = Workspace.all.last
         users = workspace.users
         users.must_be_instance_of Array
         users.first.must_be_instance_of User
+      end
+    end
+
+    describe '#tags' do
+      it "should return all tags for the given workspace" do
+        workspace = Workspace.all.last
+        tags = workspace.tags
+        tags.must_be_instance_of Array
+        tags.first.must_be_instance_of Tag
+      end
+    end
+
+    describe '#create_tag' do
+      it "should create a new tag for the given workspace" do
+        workspace = Workspace.all.last
+        task = workspace.create_tag(:name => 'asana-test-tag')
+        task.must_be_instance_of Tag
       end
     end
 
