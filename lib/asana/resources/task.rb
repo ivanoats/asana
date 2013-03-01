827a8764d984b65b187750ace94f4db97eedeacf
Ryan Bright <ryan@rbright.net>
2013-03-01 04:16:28 -0500

    Update the Task resource

--- lib/asana/resources/task.rb
+++ lib/asana/resources/task.rb
@@ -1,5 +1,59 @@
 module Asana
   class Task < Resource
+
     COLLECTION = "tasks"
+
+    class << self
+
+      def find(id)
+        get("#{COLLECTION}/#{id}")
+      end
+
+      def create(data)
+        post("#{COLLECTION}/#{id}", data)
+      end
+
+      def update(id, data)
+        put("#{COLLECTION}/#{id}", data)
+      end
+
+      def projects(id)
+        Project.get("#{COLLECTION}/#{id}/#{Project::COLLECTION}")
+      end
+
+      def stories(id)
+        Story.get("#{COLLECTION}/#{id}/#{Story::COLLECTION}")
+      end
+
+      def create_story(id, data)
+        Asana::Story.post("#{COLLECTION}/#{id}/#{Story::COLLECTION}", data)
+      end
+
+      def tags(id)
+        Tag.get("#{COLLECTION}/#{id}/#{Tag::COLLECTION}")
+      end
+
+    end
+
+    def update(data)
+      put("#{path}", data)
+    end
+
+    def projects
+      Project.get("#{path}/#{Project::COLLECTION}")
+    end
+
+    def stories
+      Story.get("#{path}/#{Story::COLLECTION}")
+    end
+
+    def create_story(data)
+      Asana::Story.post("#{path}/#{Story::COLLECTION}", data)
+    end
+
+    def tags
+      Tag.get("#{path}/#{Tag::COLLECTION}")
+    end
+
   end
 end
--- spec/asana/resources/task_spec.rb
+++ spec/asana/resources/task_spec.rb
@@ -16,18 +16,54 @@ module Asana
 
     describe '.find' do
       it 'should return a single task' do
-        task = Task.find(Workspace.all.first.tasks(:me).first.id)
+        task = Task.find(Workspace.all.last.tasks.first.id)
         task.must_be_instance_of Task
       end
     end
 
-    describe '#modify' do
-      it 'should modify the given task with a new name' do
-        task = Workspace.all.first.create_task(:name => 'asana-test-foo', :assignee => 'me')
-        task.modify(:name => 'asana-test-bar').name.must_equal 'asana-test-bar'
+    describe '.update' do
+      it "should update the given task with a new name" do
+        task = Workspace.all.last.create_task(:name => 'asana-test-foo')
+        Task.update(task.id, :name => 'asana-test-bar')
+        task = Task.find(task.id)
+        task.name.must_equal 'asana-test-bar'
       end
     end
 
+    describe '.projects' do
+      it 'should return all projects for the given task' do
+        task = Project.all.first.tasks.first
+        projects = Task.projects(task.id)
+        projects.must_be_instance_of Array
+        projects.first.must_be_instance_of Project
+      end
+    end
+
+    describe '.stories' do
+      it 'should return all stories for the given task' do
+        task = Project.all.first.tasks.first
+        stories = Task.stories(task.id)
+        stories.must_be_instance_of Array
+        stories.first.must_be_instance_of Story
+      end
+    end
+
+    describe '.create_story' do
+      it 'should create a new story for the given task' do
+        task = Project.all.first.tasks.first
+        story = Task.create_story(task.id, :text => 'foo')
+        story.must_be_instance_of Story
+      end
+    end
+
+    describe '.tags' do
+      it 'should return all tags for the given task' do
+        task = Project.all.last.tasks.first
+        tags = Task.tags(task.id)
+        tags.must_be_instance_of Array
+        tags.first.must_be_instance_of Tag
+      end
+    end
     describe '#projects' do
       it 'should return all projects for the given task' do
         task = Project.all.first.tasks.first
@@ -37,14 +73,15 @@ module Asana
       end
     end
 
-    describe '#add_project' do
-      it 'should add the project to the given task' do
-        task = Workspace.all.first.create_task(:name => 'asana-test-task-add-project', :assignee => 'me')
-        project = Workspace.all.first.create_project(:name => 'asana-test-task-parent-project')
-        task.add_project project.id
+    describe '#stories' do
+      it 'should return all stories for the given task' do
+        task = Project.all.first.tasks.first
+        stories = task.stories
+        stories.must_be_instance_of Array
+        stories.first.must_be_instance_of Story
       end
     end
-    
+
     describe '#create_story' do
       it 'should create a new story for the given task' do
         task = Project.all.first.tasks.first
@@ -53,12 +90,12 @@ module Asana
       end
     end
 
-    describe '#stories' do
-      it 'should return all stories for the given task' do
-        task = Project.all.first.tasks.first
-        stories = task.stories
-        stories.must_be_instance_of Array
-        stories.first.must_be_instance_of Story
+    describe '#tags' do
+      it 'should return all tags for the given task' do
+        task = Project.all.last.tasks.first
+        tags = task.tags
+        tags.must_be_instance_of Array
+        tags.first.must_be_instance_of Tag
       end
     end
 
